6c51ec58cd404a3931473d309ce096e7f8ba2365
Luciano Muratore <muratore2401@gmail.com>
2022-01-10 19:35:17 -0300

    Conexion a la BD

--- ubicaciones-back/index.js
+++ ubicaciones-back/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const conectarDB = require('./config/db');
 
 //Creamos el servidor
 const app = express();
+
+conectarDB(); // Conectamos a la BD
 
 app.listen(4000, () => {
     console.log('El servidor se levanto correctamente')
