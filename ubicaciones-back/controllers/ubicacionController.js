2649d8023f2270095099a7871f0e903156c14382
Luciano Muratore <muratore2401@gmail.com>
2022-01-10 20:42:19 -0300

    Listar, editar, borrar y modificar ubicaciones

--- ubicaciones-back/controllers/ubicacionController.js
+++ ubicaciones-back/controllers/ubicacionController.js
@@ -11,6 +11,72 @@ exports.crearUbicacion = async (req,res) => {
 
     } catch (error) {
         console.log(error);
-        res.status(500).send('Hubo un error. Culpa de Cintia')
+        res.status(500).send('Hubo un error.')
+    }
+}
+
+exports.listarUbicaciones = async (req,res) => {
+    try {
+        const ubicaciones = await Ubicacion.find();
+        res.json(ubicaciones);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error.')
+    }
+}
+
+exports.editarUbicacion = async (req,res) => {
+    try {
+        const { rubro, direccion, localidad } = req.body;
+        let ubicacion = await Ubicacion.findById(req.params.id);
+
+        if(!ubicacion){ 
+            res.status(404).json({ msg: 'No existe la ubicacion'})
+        }
+
+        ubicacion.rubro = rubro;
+        ubicacion.direccion = direccion;
+        ubicacion.localidad = localidad;
+
+        ubicacion = await Ubicacion.findOneAndUpdate({_id: req.params.id}, ubicacion, { new: true})
+        res.json(ubicacion);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error.')
+    }
+}
+
+exports.listarUbicacion = async (req,res) => {
+    try {
+        let ubicacion = await Ubicacion.findById(req.params.id);
+
+        if(!ubicacion){ 
+            res.status(404).json({ msg: 'No existe la ubicacion'})
+        }
+
+        res.json(ubicacion);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error.')
+    }
+}
+
+exports.eliminarUbicacion = async (req,res) => {
+    try {
+        let ubicacion = await Ubicacion.findById(req.params.id);
+
+        if(!ubicacion){ 
+            res.status(404).json({ msg: 'No existe la ubicacion'})
+        }
+
+        await Ubicacion.findOneAndRemove({_id: req.params.id})
+        res.json({msg: 'Ubicacion eliminada'});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error.')
     }
 }
--- ubicaciones-back/routes/ubicacion.js
+++ ubicaciones-back/routes/ubicacion.js
@@ -5,5 +5,9 @@ const ubicacionController = require('../controllers/ubicacionController');
 
 // api/ubicaciones
 router.post('/', ubicacionController.crearUbicacion);
+router.get('/', ubicacionController.listarUbicaciones);
+router.put('/:id', ubicacionController.editarUbicacion);
+router.get('/:id', ubicacionController.listarUbicacion);
+router.delete('/:id', ubicacionController.eliminarUbicacion);
 
 module.exports = router;
