e2c2bb6e773742bea6b8e6fc292a0e19b9354230
Cintia Capece <cintiflor@gmail.com>
2022-02-10 20:08:32 -0300

    FRONT - Genera componente mapa-ubicacion

---
+++ ubicaciones-front/src/app/components/mapa-ubicacion/mapa-ubicacion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MapaUbicacionComponent } from './mapa-ubicacion.component';
+
+describe('MapaUbicacionComponent', () => {
+  let component: MapaUbicacionComponent;
+  let fixture: ComponentFixture<MapaUbicacionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ MapaUbicacionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MapaUbicacionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
