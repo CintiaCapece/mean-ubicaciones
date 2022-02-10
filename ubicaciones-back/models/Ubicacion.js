e684f7d7cd41caca5dc66f53a1421fd9c52066da
Cintia Capece <cintiflor@gmail.com>
2022-02-10 19:16:59 -0300

    BACK - Agrega en el modelo ubicaciones los atributos latitud y longitud

--- ubicaciones-back/models/Ubicacion.js
+++ ubicaciones-back/models/Ubicacion.js
@@ -4,7 +4,9 @@ const UbicacionSchema = mongoose.Schema({
     rubro: {type: String, required: true},
     direccion: {type: String, required: true},
     localidad: {type: String, required: true},
-    fechaCreacion: {type: Date, default: Date.now()}
+    fechaCreacion: {type: Date, default: Date.now()},
+    latitud: {type: Number, required: true},
+    longitud: {type: Number, required: true},
 });
 
 module.exports = mongoose.model('Ubicacion', UbicacionSchema);
